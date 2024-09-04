16ed7e0081359932640c8189dd86f999e89447b8
H33Criss <agartkg95@gmail.com>
2024-09-04 11:32:11 -0400

    feat(pages): create new widgets to init structure

---
+++ lib/src/pages/tandas/tandas_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TandasPage extends StatelessWidget {
+  const TandasPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
