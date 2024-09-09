68b1a86babfa4f81005a3dd2fd7917c9574e0114
H33Criss <agartkg95@gmail.com>
2024-09-09 14:19:39 -0300

    fix(): add new structure in envios page

--- lib/src/pages/envios/envios_page.dart
+++ lib/src/pages/envios/envios_page.dart
@@ -19,7 +19,60 @@ class _EnviosPageState extends State<EnviosPage> {
             'accion': 'cargar>>',
             'checked': false, // Agregar estado del checkbox
           });
+  Map<String, dynamic> ejemploDataReal = {
+    //?Incialmente(Antes de apretar el boton "Iniciar nuevo envio").
+    "envioIniciado": null,
 
+    //?Luego de presionar el boton "Iniciar nuevo envio".
+    // "envioIniciado":{
+    //   "id":'7cf54f1a-0ecb-43ab-bcbb-d42764d802d0',
+    // },
+
+    //*Cuando se presione el boton "Completar el envio", el campo
+    //* "envioIniciado", vuelve a null y los checkboxs su propiedad "isComplete"
+    //* vuelven a false. Es decir al estado inicial.
+
+    "id": "4edded3a-fa78-4e35-b824-5f61645005f7",
+    "fecha": "2024-09-04",
+    "detalles": [
+      {
+        "id": "7cf54f1a-0ecb-43ab-bcbb-d42764d802d0",
+        "cantidadPlanificada": 15,
+        "categoria": "Arroz",
+        "isComplete": false, //Indica si es el checkbox esta "checked" o no.
+        "categoriaId": "f0b1ac2a-a958-40c1-95f0-ff48890b6199",
+        "urlImagen":
+            "https://www.lafallera.es/wp-content/uploads/2023/04/Cocción-para-el-arroz-largo-pasos-y-trucos.jpg"
+      },
+      {
+        "id": "5bdf1f30-c35b-4d5f-a319-b392376e581d",
+        "cantidadPlanificada": 88,
+        "categoria": "Panadería",
+        "isComplete": false, //Indica si es el checkbox esta "checked" o no.
+        "categoriaId": "c14d0c57-c188-43fd-b6d2-8db7d6114b05",
+        "urlImagen":
+            "https://thefoodtech.com/wp-content/uploads/2023/10/PANADERIA-PRINCIPAL-1.jpg"
+      },
+      {
+        "id": "4f0991c5-d895-4293-8053-2a775f1505b6",
+        "cantidadPlanificada": 25,
+        "categoria": "Bebidas",
+        "isComplete": false, //Indica si es el checkbox esta "checked" o no.
+        "categoriaId": "54f1a538-2c2b-42c1-b6a6-ab4d360b73d2",
+        "urlImagen":
+            "https://www.dietdoctor.com/wp-content/uploads/2017/04/Guide_Drinks_16x9b.jpg"
+      },
+      {
+        "id": "7c4194a5-6523-4b87-bfbb-2ab07a020518",
+        "cantidadPlanificada": 150,
+        "categoria": "Cubiertos",
+        "isComplete": false, //Indica si es el checkbox esta "checked" o no.
+        "categoriaId": "dee88b0b-0f38-4f10-87fa-dbf972038595",
+        "urlImagen":
+            "https://eurohome.cl/cdn/shop/products/0302.914-5_1800x.jpg?v=1637683715"
+      }
+    ]
+  };
   bool envioIniciado = false;
 
   bool _todosSeleccionados() {
